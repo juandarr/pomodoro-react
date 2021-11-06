fc4b3741e3701648d3e03d5b4000b862af32bc5d
Juan Ríos <juandarr@gmail.com>
2021-11-06 03:05:53 -0500

    add: main files of pomodoro app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,189 @@
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
+import React from 'react';
+
+const events = { 0: "session", 1: "break" };
+
+//Title component
+const Title = (props) => (<div id="title">
+  <h1><em>my</em>{props.name}</h1>
+</div>);
+
+class EventLength extends React.Component {
+
+  render() {
+    const event = this.props.event;
+    return (<div id={event + "-control"} className="d-flex flex-column align-items-center">
+      <h2 id={event + "-label"}>{event[0].toUpperCase() + event.slice(1)} <em>length</em></h2>
+      <div id={event + "-buttons"} className="d-flex justify-content-evenly align-items-center">
+        <button className="btn-link" id={event + "-increment"} data-bs-toggle="tooltip" data-bs-placement="right"
+          title={event[0].toUpperCase() + event.slice(1) + " time up"} onClick={this.props.eventTimeController} disabled={this.props.playState} value='+'>
+          <img src="resources/arrow-up.svg" alt={event + " time up"} />
+        </button>
+        <div id={event + "-length"} className="d-flex justify-content-center">{this.props.timeLength}</div>
+        <button className="btn-link" id={event + "-decrement"} data-bs-toggle="tooltip" data-bs-placement="right"
+          title={event[0].toUpperCase() + event.slice(1) + " time down"} onClick={this.props.eventTimeController} disabled={this.props.playState} value='-'>
+          <img className="down" src="resources/arrow-up.svg" alt={event + " time down"} />
+        </button>
+      </div></div>);
+  }
+}
+
+class EventsController extends React.Component {
+  render() {
+    return (<div id="controllers" className="d-flex justify-content-evenly">
+      <EventLength event="break" timeLength={this.props.breakLength} eventTimeController={this.props.eventTimeController} playState={this.props.playState} />
+      <EventLength event="session" timeLength={this.props.sessionLength} eventTimeController={this.props.eventTimeController} playState={this.props.playState} />
+    </div>);
+  }
+}
+
+class TimerDisplay extends React.Component {
+
+  timeRender(ar) {
+    return (ar[0] < 10 ? "0" + ar[0] : ar[0]) + ":" + (ar[1] < 10 ? "0" + ar[1] : ar[1]);
+  }
+
+  render() {
+    const event = this.props.events[this.props.eventIndex];
+    return (<div id="timer" className="d-flex flex-column align-items-center justify-content-center">
+      <h2 id="timer-label">{event[0].toUpperCase() + event.slice(1)}</h2>
+      <div id="time-left">{this.timeRender(this.props.timeLeft)}</div>
+    </div>);
+  }
+}
+TimerDisplay.defaultProps = { events: events };
+
+const Alarm = (props) => (<audio id="beep" src={props.alarmFile} className="alarm-timer"></audio>);
+
+
+class TimerController extends React.Component {
+
+  render() {
+    return (<div id="timer-controls" className="d-flex flex-column justify-content-evenly">
+      <button className="btn-link" data-bs-toggle="tooltip" data-bs-placement="right" id="start_stop"
+        title={(this.props.playState ? "Pause" : "Start") + " timer"} onClick={this.props.timerPlayPause}><img src={"resources/" + (this.props.playState ? "pause" : "play") + ".svg"} alt="play-pause button" /></button>
+      <button className="btn-link" data-bs-toggle="tooltip" data-bs-placement="right" id="reset"
+        title="Reset timer" onClick={this.props.timerReset}><img src="resources/reset.svg" alt="reset button" /></button>
+      <Alarm alarmFile="resources/bell-tower.mp3" />
+    </div>);
+  }
+}
+
+class Timer extends React.Component {
+  render() {
+    return (<div id="timer-container" className="d-flex justify-content-evenly">
+      <TimerDisplay eventIndex={this.props.eventIndex} timeLeft={this.props.timeLeft} />
+      <TimerController playState={this.props.playState} timerReset={this.props.timerReset} timerPlayPause={this.props.timerPlayPause} />
+    </div>);
+  }
+}
+
+class Pomodoro extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.props.defaultState;
+    this.eventTimeController = this.eventTimeController.bind(this);
+    this.timerReset = this.timerReset.bind(this);
+    this.timerPlayPause = this.timerPlayPause.bind(this);
+    this.timer = this.timer.bind(this);
+  }
+
+  componentDidMount() {
+    this.sound = document.getElementById('beep');
+  }
+
+  componentDidUpdate() {
+    //Stop the timer when the pause button is pressed
+    if (!this.state.playState) {
+      clearInterval(this.state.timerId);
+    }
+    //Start new event in case the flag newEvent has been set to true
+    if (this.state.newEvent) {
+
+      setTimeout(() => {
+        this.setState((state) => {
+          const newIndex = (state.eventIndex + 1) % 2;
+          let newTimeLeft;
+          if (this.props.events[newIndex] === 'break') {
+            newTimeLeft = [state.breakLength, 0];
+          } else if (this.props.events[newIndex] === 'session') {
+            newTimeLeft = [state.sessionLength, 0];
+          }
+          return { eventIndex: newIndex, timeLeft: newTimeLeft, newEvent: false };
+        });
+        this.timer();
+      }, 1000);
+    }
+  }
+
+  timer() {
+    const timerId = setInterval(() => {
+      this.setState((state) => {
+        if (state.timeLeft[1] === 0 && state.timeLeft[0] > 0) {
+          return { timeLeft: [state.timeLeft[0] - 1, 59] };
+        } else if (state.timeLeft[1] === 1 && state.timeLeft[0] === 0) {
+          this.sound.play();
+          clearInterval(this.state.timerId)
+          return { timeLeft: [0, 0], newEvent: true };
+        } else if (state.timeLeft[1] > 0) {
+          return { timeLeft: [state.timeLeft[0], state.timeLeft[1] - 1] };
+        }
+      });
+    }, 1000);
+    this.setState({ timerId: timerId });
+  }
+
+
+  timerReset() {
+    this.sound.pause();
+    this.sound.currentTime = 0;
+    clearInterval(this.state.timerId);
+    this.setState(this.props.defaultState);
+  }
+
+  timerPlayPause() {
+    const newPlayState = !this.state.playState;
+    if (newPlayState) { this.timer(); };
+    this.setState({ playState: newPlayState });
+  }
+
+  eventTimeController(event) {
+    const valueEvent = event.target.value;
+    const lengthEvent = event.target.id.split('-')[0] === 'break' ? 'breakLength' : 'sessionLength';
+    const currentEvent = this.props.events[this.state.eventIndex] + 'Length';
+    this.setState((state) => {
+      const newState = {};
+      let newLength;
+      if (valueEvent === '+') {
+        newLength = state[lengthEvent] <= 59 ? (state[lengthEvent] + 1) : state[lengthEvent];
+      } else {
+        newLength = state[lengthEvent] > 1 ? (state[lengthEvent] - 1) : state[lengthEvent];
+      }
+      newState[lengthEvent] = newLength;
+      if (lengthEvent === currentEvent) {
+        newState['timeLeft'] = [newLength, 0];
+      }
+      return newState;
+    });
+  }
+
+  render() {
+    return (<div id="pomodoro" className="d-flex flex-column align-items-center justify-content-evenly">
+      <Title name="Pomodoro" />
+      <EventsController breakLength={this.state.breakLength} sessionLength={this.state.sessionLength} eventTimeController={this.eventTimeController} playState={this.state.playState} />
+      <Timer timeLeft={this.state.timeLeft} playState={this.state.playState} eventIndex={this.state.eventIndex} timerReset={this.timerReset} timerPlayPause={this.timerPlayPause} />
+    </div>);
+  }
+}
+
+Pomodoro.defaultProps = { defaultState: { breakLength: 5, sessionLength: 25, eventIndex: 0, timeLeft: [25, 0], playState: false, newEvent: false, timerId: '' }, events: events };
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pomodoro />
     </div>
   );
 }
