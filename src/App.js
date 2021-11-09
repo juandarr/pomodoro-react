b00efb6f3e1961750db16e1e0e4697d934170746
Juan Ríos <juandarr@gmail.com>
2021-11-09 01:00:47 -0500

    add: new menu tab for settings and analytics modals; add: created UI for settings, added color themes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import './bootstrap.min.css';
-import './bootstrap-icons.css';
 import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+//Resources
+//Color themes currently available in the app
+const colorThemesArray = ['totalDark', 'passionVibes', 'seriousMood'];
+//Sound alarms currently available in the app
+const alarmSound = [];
 
-/**
- * TODO: There is a problem with the up arrow images. The culprit is found in the img section
- * of the respective up-down buttons, for both kind of events (break and session). 
- * When defining an image embedded in a button, it is selected as the event.target when clicking over the button. 
- * Weird behaviors
- * DONE: The problem are not the images or event.target, but the test definitions
- */
 
 const events = { 0: "session", 1: "break" };
+
+//Main icons
+const UpArrowIcon = (props) => (<svg className={"iconColor " + props.down} xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill={props.color}><g><rect fill="none" height="24" width="24" /></g><g><g><polygon points="6,17.59 7.41,19 12,14.42 16.59,19 18,17.59 12,11.59" /><polygon points="6,11 7.41,12.41 12,7.83 16.59,12.41 18,11 12,5" /></g></g></svg>);
+
+const PlayIcon = (props) => (<svg className={"iconColor"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill={props.color}><path d="M0 0h24v24H0V0z" fill="none" /><path d="M10 16.5l6-4.5-6-4.5zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" /></svg>);
+
+const PauseIcon = (props) => (<svg className={"iconColor"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill={props.color}><path d="M0 0h24v24H0V0z" fill="none" /><path d="M13 8h2v8h-2zM9 8h2v8H9zm3 14c5.52 0 10-4.48 10-10S17.52 2 12 2 2 6.48 2 12s4.48 10 10 10zm0-18c4.41 0 8 3.59 8 8s-3.59 8-8 8-8-3.59-8-8 3.59-8 8-8z" /></svg>);
+
+const ResetIcon = (props) => (<svg className={"iconColor"} xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill={props.color}><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z" /><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z" /></g></g></svg>);
 
 //Title component
 const Title = (props) => (<div id="title">
@@ -25,18 +32,20 @@ class EventLength extends React.Component {
     const event = this.props.event;
     const color = this.props.color;
     return (<div id={event + "-control"} className="d-flex flex-column align-items-center">
-      <h2 className={'stroke-thin event-label ' + color}>{event[0].toUpperCase() + event.slice(1)} <em>length</em></h2>
+      <h2 id={event + "-label"} className={'stroke-thin event-label ' + color}>{event[0].toUpperCase() + event.slice(1)} <em>length</em></h2>
       <div id={event + "-buttons"} className="d-flex justify-content-evenly align-items-center event-buttons">
         <button className="d-flex justify-content-center align-items-center btn-link"
           id={event + "-increment"} data-bs-toggle="tooltip" data-bs-placement="right"
-          title={event[0].toUpperCase() + event.slice(1) + " time up"} onClick={this.props.eventTimeController} disabled={this.props.playState} value='+'>
-          <img src="resources/arrow-up.svg" alt={event + " time up"} />
+          title={event[0].toUpperCase() + event.slice(1) + " time up"} onClick={this.props.eventTimeController} disabled={this.props.playState || this.props.settingsOpen} value='+'>
+          <UpArrowIcon color="#000000" />
+          {/*<img src="resources/arrow-up.svg" alt={event + " time up"} />*/}
         </button>
         <div id={event + "-length"} className={"d-flex justify-content-center event-length stroke-thin " + color}>{this.props.timeLength}</div>
         <button className="d-flex justify-content-center align-items-center btn-link"
           id={event + "-decrement"} data-bs-toggle="tooltip" data-bs-placement="right"
-          title={event[0].toUpperCase() + event.slice(1) + " time down"} onClick={this.props.eventTimeController} disabled={this.props.playState} value='-'>
-          <img src="resources/arrow-down.svg" alt={event + " time down"} />
+          title={event[0].toUpperCase() + event.slice(1) + " time down"} onClick={this.props.eventTimeController} disabled={this.props.playState || this.props.settingsOpen} value='-'>
+          {/*<img src="resources/arrow-down.svg" alt={event + " time down"} />*/}
+          <UpArrowIcon color="#000000" down="down" />
         </button>
       </div></div>);
   }
@@ -45,8 +54,8 @@ class EventLength extends React.Component {
 class EventsController extends React.Component {
   render() {
     return (<div id="controllers" className="d-flex justify-content-evenly">
-      <EventLength event="break" color="break-tc" timeLength={this.props.breakLength} eventTimeController={this.props.eventTimeController} playState={this.props.playState} />
-      <EventLength event="session" color="session-tc" timeLength={this.props.sessionLength} eventTimeController={this.props.eventTimeController} playState={this.props.playState} />
+      <EventLength event="break" color="break-tc" timeLength={this.props.breakLength} eventTimeController={this.props.eventTimeController} playState={this.props.playState} settingsOpen={this.props.settingsOpen} />
+      <EventLength event="session" color="session-tc" timeLength={this.props.sessionLength} eventTimeController={this.props.eventTimeController} playState={this.props.playState} settingsOpen={this.props.settingsOpen} />
     </div>);
   }
 }
@@ -75,9 +84,9 @@ class TimerController extends React.Component {
   render() {
     return (<div id="timer-controls" className="d-flex flex-column justify-content-evenly">
       <button className="d-flex justify-content-center align-items-center btn-link" data-bs-toggle="tooltip" data-bs-placement="right" id="start_stop"
-        title={(this.props.playState ? "Pause" : "Start") + " timer"} onClick={this.props.timerPlayPause}><img src={"resources/" + (this.props.playState ? "pause" : "play") + ".svg"} alt="play-pause button" /></button>
+        title={(this.props.playState ? "Pause" : "Start") + " timer"} onClick={this.props.timerPlayPause} disabled={this.props.settingsOpen}>{this.props.playState ? (<PauseIcon color="#000000" />) : (<PlayIcon color="#000000" />)}</button>
       <button className="d-flex justify-content-center align-items-center btn-link" data-bs-toggle="tooltip" data-bs-placement="right" id="reset"
-        title="Reset timer" onClick={this.props.timerReset}><img src="resources/reset.svg" alt="reset button" /></button>
+        title="Reset timer" onClick={this.props.timerReset} disabled={this.props.settingsOpen}><ResetIcon color="#000000" /></button>
       <Alarm alarmFile="resources/bell-tower.mp3" />
     </div>);
   }
@@ -87,7 +96,7 @@ class Timer extends React.Component {
   render() {
     return (<div id="timer-container" className="d-flex justify-content-evenly">
       <TimerDisplay eventIndex={this.props.eventIndex} timeLeft={this.props.timeLeft} />
-      <TimerController playState={this.props.playState} timerReset={this.props.timerReset} timerPlayPause={this.props.timerPlayPause} />
+      <TimerController playState={this.props.playState} timerReset={this.props.timerReset} timerPlayPause={this.props.timerPlayPause} settingsOpen={this.props.settingsOpen} />
     </div>);
   }
 }
@@ -101,6 +110,8 @@ class Pomodoro extends React.Component {
     this.timerReset = this.timerReset.bind(this);
     this.timerPlayPause = this.timerPlayPause.bind(this);
     this.timer = this.timer.bind(this);
+    this.toggleSettings = this.toggleSettings.bind(this);
+    this.selectTheme = this.selectTheme.bind(this);
   }
 
   componentDidMount() {
@@ -163,8 +174,8 @@ class Pomodoro extends React.Component {
   }
 
   eventTimeController(event) {
-    const valueEvent = event.target.parentElement.value;
-    const lengthEvent = event.target.parentElement.id.split('-')[0] === 'break' ? 'breakLength' : 'sessionLength';
+    const valueEvent = event.target.value;
+    const lengthEvent = event.target.id.split('-')[0] === 'break' ? 'breakLength' : 'sessionLength';
     const currentEvent = this.props.events[this.state.eventIndex] + 'Length';
     this.setState((state) => {
       const newState = {};
@@ -182,20 +193,70 @@ class Pomodoro extends React.Component {
     });
   }
 
+  toggleSettings(event) {
+    const tmp = event.target;
+    const settings = document.getElementById('tab-view');
+    if (settings.style.visibility === 'hidden') {
+      settings.style.visibility = 'visible';
+      tmp.style.setProperty("border-top-left-radius", '0px', 'important');
+      tmp.classList.add('settingsTheme');
+      this.setState({ settingsOpen: true });
+    } else {
+      settings.style.visibility = 'hidden';
+      tmp.style.setProperty("border-top-left-radius", '0.9vmin', 'important');
+      tmp.classList.remove('settingsTheme');
+      this.setState({ settingsOpen: false });
+    }
+  }
+
+  selectTheme(event) {
+    this.setState({ colorSet: event.target.value });
+  }
+
   render() {
-    return (<div id="pomodoro" className="d-flex flex-column align-items-center justify-content-evenly colorSet-seriousMood">
-      <Title name="Pomodoro" />
-      <EventsController breakLength={this.state.breakLength} sessionLength={this.state.sessionLength} eventTimeController={this.eventTimeController} playState={this.state.playState} />
-      <Timer timeLeft={this.state.timeLeft} playState={this.state.playState} eventIndex={this.state.eventIndex} timerReset={this.timerReset} timerPlayPause={this.timerPlayPause} />
+    return (<div className="d-flex justify-content-between">
+      <div id="pomodoro" className={"d-flex justify-content-evenly colorSet-" + this.state.colorSet}>
+        <div id="pomodoro-view" className="d-flex flex-column align-items-center justify-content-evenly">
+          <Title name="Pomodoro" />
+          <EventsController breakLength={this.state.breakLength} sessionLength={this.state.sessionLength} eventTimeController={this.eventTimeController} playState={this.state.playState} settingsOpen={this.state.settingsOpen} />
+          <Timer timeLeft={this.state.timeLeft} settingsOpen={this.state.settingsOpen}
+            playState={this.state.playState} eventIndex={this.state.eventIndex} timerReset={this.timerReset} timerPlayPause={this.timerPlayPause} />
+        </div>
+        <div id="pomodoro-menu" className="btn-group-vertical" role="group" aria-label="First group">
+          <button onClick={this.toggleSettings} className="d-flex justify-content-center align-items-center btn-link tab btn-tab-start" ><i className="bi bi-gear-wide-connected"></i></button>
+          <button className="d-flex justify-content-center align-items-center btn-link tab btn-tab-end"><i className="bi bi-graph-up"></i></button>
+        </div>
+        <div id="tab-view" style={{ visibility: 'visible' }} className="d-flex flex-column justify-content-start align-items-center">
+          <h2 className="stroke-thick settings-title">Settings</h2>
+          <div id="features">
+            <div id="colorTheme" className="feature d-flex justify-content-between align-items-center">
+              <h3 className="stroke-thin event-label pomodoro-tc2 feature-text">Color theme</h3>
+              <select name="colorThemes" id="theme" onChange={this.selectTheme}>
+                <option value='totalDark'>totalDark</option>
+                <option value='seriousMood'>seriousMood</option>
+                <option value='passionVibes'>passionVibes</option>
+              </select>
+            </div>
+            <div id="sessionAlarm" className="feature d-flex justify-content-between align-items-center">
+              <h3 className="stroke-thin event-label session-tc feature-text">Session alarm</h3>
+              <select></select>
+            </div>
+            <div id="breakAlarm" className="feature d-flex justify-content-between align-items-center">
+              <h3 className="stroke-thin event-label break-tc feature-text">Break alarm</h3>
+              <select></select>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>);
   }
 }
 
-Pomodoro.defaultProps = { defaultState: { breakLength: 5, sessionLength: 25, eventIndex: 0, timeLeft: [25, 0], playState: false, newEvent: false, timerId: '' }, events: events };
+Pomodoro.defaultProps = { defaultState: { breakLength: 5, sessionLength: 25, eventIndex: 0, timeLeft: [25, 0], playState: false, newEvent: false, timerId: '', settingsOpen: false, analyticsOpen: false, colorSet: 'totalDark' }, events: events };
 
 function App() {
   return (
-    <div className="App">
+    <div className="App d-flex justify-content-between">
       <Pomodoro />
     </div>
   );
